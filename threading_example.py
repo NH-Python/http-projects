58ed3ec033e6a7066e4fb9e4ce5fca5b97a459d6
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-12-16 11:47:46 -0500

    Update threading_example.py

--- threading_example.py
+++ threading_example.py
@@ -6,12 +6,12 @@ import threading
 def make_request():
     
     url="https://www.ruqqus.com"
-    response=request.get(url)
+    response=requests.get(url)
     
     print(response.status_code)
     
-def test_the_server():
+def test_the_server(n=100):
 
-    for i in range(100):
+    for i in range(n):
         new_thread=threading.Thread(target=make_request)
         new_thread.start()
